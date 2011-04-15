75522fc6ffe087192bf6abf49b02c24a7f8cb98e
lfborjas <luis.borjas@escolarea.com>
2011-04-15 12:46:38 -0600

    added uuid dependency

--- eddie/project.clj
+++ eddie/project.clj
@@ -1,4 +1,6 @@
 (defproject eddie "1.0.0-SNAPSHOT"
   :description "FIXME: write"
   :dependencies [[org.clojure/clojure "1.2.0"]
-                 [org.clojure/clojure-contrib "1.2.0"]])
+                 [org.clojure/clojure-contrib "1.2.0"]
+                 [com.eaio.uuid/uuid "3.2"]
+                 ])
