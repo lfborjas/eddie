dd959173dbf6bfb5fee323ae7961ed331dec4eb1
lfborjas <luis.borjas@escolarea.com>
2011-04-28 20:17:59 -0600

    hay rollo con la duplicación de la response

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
-/*Capa de Presentación*/
-
-var fs = require('fs')
-
 /*Capa de Datos*/
+
+Object.prototype.lookup = function(key, deflt){
+    return this.hasOwnProperty(key)? this[key] : deflt
+}
 
 //objeto global donde guardaremos los messages 
 var Channel = new function(){
@@ -36,82 +36,23 @@ var Channel = new function(){
 }
 /*Capa de Aplicación*/
 
-var http = require('http'),
-    qs  = require('querystring'),
-    url = require('url')
+var eddie = require('./eddie'),
+    app = new eddie.Application();
 
-var application = function(request, response){
-    //1. Analizar la solicitud:
-    var urlInfo = url.parse(request.url),
-        GET = qs.parse(urlInfo.query),
-        path = urlInfo.pathname
+app.match('^/[a-zA-Z]+\\.(js|css)$', eddie.staticHandler)
+app.match('^/$', eddie.respondWithFile("index.html"))
+
+app.match('^/messages(/recv)?$', function(request, response){
+    Channel.getMessages(parseInt(request.GET.lookup('since', '0')), function(messages){
+        response.asJSON({messages: messages})
+    })    
+})
     
-    //un poco de abstracción: una función que sabe cómo construir la respuesta 
-    var doResponse = function(res){
-        var body = res.type == 'application/json'?
-                        JSON.stringify(res.body) :
-                        res.body || "";
+app.match('^/messages/send$', function(request, response){
+    var txt;
+    if(txt = request.GET.lookup('text', ''))
+        Channel.addMessage(txt)
+    response.asJSON({agregado: true})
+})
 
-        response.writeHead(res.status || 200, {
-            'Content-type': res.type,
-            'Content-Length': body.length
-        })
-        response.end(body)
-    }
-    
-    //2. Construir body y doResponse
-    var result = {type: "text/html"}
-    if(path === '/'){
-        result.body = fs.readFileSync("index.html", 'utf-8')
-        doResponse(result)
-
-    }else if(path.indexOf('.js') != -1){
-        //está pidiendo algún .js
-        result.body = fs.readFileSync(path.slice(1), 'utf-8')
-        result.type = "application/javascript"
-        doResponse(result)
-        
-    }else if(matches = path.match(/\/messages(\/[a-z]+)?/)){
-        //son de la forma /messages/ACCION
-        var action = matches[1] && matches[1].slice(1) || "recv";
-
-        switch(action){
-            case "recv":
-                /*¡Función anónima como parámetro!
-                  Usa la propiedad de cierre (closure)
-                  Será llamada cuando los messages estén listos
-                 */
-                Channel.getMessages(parseInt(GET.since || '0'), function(messages){
-                    result.type = 'application/json'
-                    /*la función map devuelve una lista con la función
-                      provista aplicada a cada elemento de la colección
-                     */
-                    result.body = {messages: messages}
-                    doResponse(result)
-                })    
-                break;
-            case "send":
-                if(GET.text)
-                    Channel.addMessage(GET.text)
-                result.type = "application/json"
-                result.body = {agregado : true}
-                doResponse(result)
-                break;
-            default:
-                result.body = "Acción inválida"
-                result.status = 404
-                doResponse(result)
-        }
-    }else{
-        result.body = "Desconocido"
-        result.status = 404
-        doResponse(result)
-    }
-}
-
-/*crear un daemon que escucha por solicitudes y sabe doResponse
-  le pasará control a la función `application`
-  */
-var daemon = http.createServer(application)
-daemon.listen(6660, "127.0.0.1")
-console.log("El servidor está en http://127.0.0.1:6660")
+app.serve()
--- eddie.js
+++ eddie.js
@@ -1,7 +1,7 @@
 var http = require('http'),
     qs  = require('querystring'),
     url = require('url'),
-    fs = require('fs')
+    fs = require('fs');
 
 //partial function application: http://ejohn.org/blog/partial-functions-in-javascript/
 Function.prototype.partial = function(){
@@ -9,7 +9,6 @@ Function.prototype.partial = function(){
         args = Array.prototype.slice.call(arguments)
     
     return function(){
-        var arg = 0
         for (var i = 0, arg = 0; i < args.length && arg < arguments.length ; i++)
             if (args[i] === undefined)
                 args[i] = arguments[arg++];
@@ -17,7 +16,7 @@ Function.prototype.partial = function(){
         return fn.apply(this, args)
     }
 }
-
+/*Agregar las propiedades de un objeto a otro*/
 Object.prototype.merge = function(other){
     for(var prop in other){
         //por el prototipo
@@ -26,36 +25,43 @@ Object.prototype.merge = function(other){
     }
 }
 
+
+/*Abstracción de una solicitud*/
 var Request = function(req){
     this.merge(req)
     this.merge(url.parse(req.url))
     this.GET = qs.parse(this.query)
 }
 
+/*Abstracción general de respuestas*/
 var Response = function(res, body, status, type, headers){
     body = type === "application/json"?
            JSON.stringify(body):
            body || ""
-           
     headers.merge({
         'Content-type': type,
         'Content-Length': body.length
     })            
     
+    //recibe un objeto que sí sabe cómo terminar la respuesta
     res.writeHead(status, headers)
     res.end(body)
 }
 
+//"Subclases" de response: aplicaciones parciales de la función, son otras funciones
 var HTMLResponse = Response.partial(undefined, undefined, 200, "text/html", {}),
-    JSONResponse = Response.partial(undefined, undefined, 200, "application/json", {})
+    JSONResponse = Response.partial(undefined, undefined, 200, "application/json", {}),
+    NotFound     = Response.partial(undefined, "Not Found", 404, "text/plain", {})
 
-exports.respondWithFile = function(req, res, filename){
+//En node.js, la variable global `exports` es lo que se podrá importar de este módulo en otros
+exports.staticHandler = function(req, res, filename){
+      filename = filename.indexOf('/') ? filename: filename.slice(1)
       var index = filename.lastIndexOf("."),
           ext   = index < 0 ? "" : filename.substring(index),
           types = {
-              'html': "text/html",
-              'js': "application/javascript",
-              'css': "text/css"
+              '.html': "text/html",
+              '.js': "application/javascript",
+              '.css': "text/css"
           },
           contents = fs.readFileSync(filename, 'utf-8');
       res.writeHead(200,{
@@ -65,6 +71,13 @@ exports.respondWithFile = function(req, res, filename){
       res.end(contents)
 }
 
+exports.respondWithFile = function(filename){
+    return exports.staticHandler.partial(undefined, undefined, filename)
+}
+
+/*Función constructora, todo lo que asocie a `this`
+  adentro será parte del objeto construido.
+ */
 exports.Application = function(port, host, debug){
     port = port || 6660
     host = host || '127.0.0.1'
@@ -77,15 +90,24 @@ exports.Application = function(port, host, debug){
     } 
 
     var dispatch = function(req, response){
-        var request = new Request(req)
-        response.asHTML = HTMLResponse.partial(response)
-        response.asJSON = JSONResponse.partial(response)
+        var request = new Request(req),
+            matches = null,
+            handler = NotFound.partial(response)
+        response.asHTML = HTMLResponse.partial(response, undefined)
+        response.asJSON = JSONResponse.partial(response, undefined)
+        for(var route in handlers){
+            if(matches = (new RegExp(route)).exec(request.pathname)){
+                handler = handlers[route]
+                break;
+            }
+        }
         if(debug)
             console.log("Requested: "+request.pathname)
-        for(var route in handlers){
-            if(matches = (new RegExp(route)).exec(req.pathname))
-                handlers[route].apply(this, [request, response].concat(matches))
-        }
+        /*
+           Nótese que no todas las funciones van a querer tantos parámetros
+           pero a js no le importa cuántos parámetros le mandemos a una función...
+        */
+        handler.apply(this, [request, response].concat(matches))
     } 
 
     this.serve = function(){
